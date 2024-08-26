da2a2295246a6b83b22cd43f837401296db0d2d1
claurennt <c.benedetti1989@gmail.com>
2024-08-26 11:13:22 +0200

    refactor: update Information component

--- src/Components/Information/index.tsx
+++ src/Components/Information/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
 type InformationProp = {
-  dataType: string[];
   chosenResourceCount: number;
   chosenResource: string;
 };
 export const Information = ({
-  dataType,
   chosenResourceCount,
   chosenResource,
 }: InformationProp) => {
-  return dataType.length ? (
+  return (
     <>
       <h3>It seems like you have selected {chosenResource}</h3>
       <h4>
@@ -17,5 +14,5 @@ export const Information = ({
         {chosenResourceCount} {chosenResource}?
       </h4>
     </>
-  ) : null;
+  );
 };
