f7fc18b6977704f9f3d7860c758116d322eab770
claurennt <c.benedetti1989@gmail.com>
2024-08-26 11:15:42 +0200

    refactor: update MoreInfoButton component

--- src/Components/MoreInfoButton/index.tsx
+++ src/Components/MoreInfoButton/index.tsx
@@ -4,18 +4,20 @@ import React from 'react';
 type MoreInfoButtonProps = {
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   value: string | string[];
-  variant: 'outlined' | 'contained';
   topic: string;
 };
 export const MoreInfoButton = ({
   onClick,
   value,
-  variant,
   topic,
 }: MoreInfoButtonProps) => (
-  <Button onClick={onClick} variant={variant} data-url={JSON.stringify(value)}>
-    {variant === 'contained'
-      ? `Discover all related ${topic}`
-      : `Discover more info about ${topic}`}
+  <Button
+    onClick={onClick}
+    variant={typeof value === 'string' ? 'outlined' : 'contained'}
+    data-url={JSON.stringify(value)}
+  >
+    {typeof value === 'string'
+      ? `Discover more info about ${topic}`
+      : `Discover all related ${topic}`}
   </Button>
 );
