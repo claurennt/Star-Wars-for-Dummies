fd49cdefa00ddc77728604df000b881f9341c63c
claurennt <c.benedetti1989@gmail.com>
2024-08-26 11:12:47 +0200

    refactor: remove unued components and create localStorage and url utils

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,6 +1,4 @@
 export * from './ResourceCard';
 export * from './ResourcesList';
-export * from './Error';
-export * from './Loading';
 export * from './Information';
 export * from './MoreInfoButton';
